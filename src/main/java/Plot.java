ab60adf31da2ec1abc69ff024cd51cd972e762a7
Trevor Dolbow <tdolbow@udel.edu>
2021-04-05 14:54:23 -0400

    [Feature] Added methods and variables as described in UML

---
+++ src/main/java/Plot.java
@@ -0,0 +1,22 @@
+public class Plot{
+	List <Plant> reccomendedPlants;
+	List <Plant> plantsInPlot;
+	Settings options;
+	List <Point> coordinates;
+	
+	
+	public void addPoint(Point point) {
+		coordinates.add(point);
+		
+	}
+	
+	public void removePlant() {
+		
+	}
+	
+
+	
+	
+	
+	
+}
