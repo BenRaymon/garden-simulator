2c28b52e161038742adb04c56002f9964e0cb659
BenRaymon <bendraymon@gmail.com>
2021-04-05 14:21:41 -0400

    feat:stubs for report and shopping list views

--- src/main/java/ReportView.java
+++ src/main/java/ReportView.java
@@ -1,10 +1,18 @@
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class ReportView extends View{
 
+	private Text title;
+	private Text summary; 
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
+		
+	}
+	
+	public void displayReport(String title, String summary) {
 		
 	}
 	
--- src/main/java/ShoppingListView.java
+++ src/main/java/ShoppingListView.java
@@ -1,10 +1,24 @@
+import java.util.*;
+
+import javafx.scene.control.ListView;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class ShoppingListView extends View{
+	
+	private Text title;
+	private Text budget;
+	private Text cost;
+	private Text plantCount;
+	private ListView plants;
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
+		
+	}
+	
+	public void display(String title, float budget, float cost, int plantCount, HashMap plantsInGarden) {
 		
 	}
 	
