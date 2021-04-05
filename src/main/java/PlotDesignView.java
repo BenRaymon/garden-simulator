2ddc82bd342526bc7ef7505121e75ed66acef9dd
BenRaymon <bendraymon@gmail.com>
2021-04-05 14:10:28 -0400

    feat: finished stub for plotDesignView

--- src/main/java/PlotDesignView.java
+++ src/main/java/PlotDesignView.java
@@ -1,10 +1,28 @@
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class PlotDesignView extends View{
+	
+	private Slider sunlight;
+	private Slider moisture;
+	private Slider soilType; 
+	private Button drawPlot;
+	private TextField budget;
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
+		
+	}
+	
+	public void fillPlot(Color color) {
+		
+	}
+	
+	public void drawCoords() {
 		
 	}
 	
