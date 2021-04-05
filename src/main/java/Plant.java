30db677c6935fe3e1e778345aced505be0017667
mjcarr <mjcarr@udel.edu>
2021-04-05 14:20:40 -0400

    adding garden clas files

---
+++ src/main/java/Plant.java
@@ -0,0 +1,22 @@
+public class Plant{
+	
+	Settings options;
+	double spreadRadius;
+	ArrayList<Lep> lepsSupported;
+	String color;
+	String size;
+	double cost;
+	String scientificName;
+	String commonName;
+	Point position;
+	
+	public double getspreadRadius{
+		return spreadRadius;
+	}
+	
+	public void setspreadRadius(Point sr){
+		spreadRadius = sr;
+	}
+	
+	
+}
---
+++ src/main/java/Point.java
@@ -0,0 +1,38 @@
+public class Point{
+	
+	double xCord;
+	double yCord;
+	
+	public Point{
+		
+	}
+	
+	public int getxCord() {
+		return xCord;
+	}
+	
+	public int getyCord() {
+		return yCord;
+	}
+	
+	public int setxCord(int x) {
+		xCord = x;
+	}
+	
+	public int setyCord(int y) {
+		yCord = y;
+	}
+	
+	public int isValid(){
+		
+	}
+	
+	public boolean checkBoundry() {
+		
+	}
+	
+	public boolean isValid() {
+		
+	}
+	
+}
