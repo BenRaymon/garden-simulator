7038f407ce2acc0b456655734f872c6c41364e0a
gavinbm <gaving1226@gmail.com>
2021-04-07 22:00:43 -0400

    test : added GardenEditorModel and PlotDesignModel tests

---
+++ src/test/java/GardenEditorModelTest.java
@@ -0,0 +1,41 @@
+import static org.junit.Assert.*;
+
+public class GardenEditorModelTest {
+	
+	@Test
+	public void testselectPlant() {
+		//fail("Not yet implemented");
+		assertEquals(null,null);
+	}
+	
+	@Test
+	public void testplacePlant() {
+		//fail("Not yet implemented");
+		assertEquals(0,0);
+	}
+
+	@Test
+	public void testisValidPlacement() {
+		//fail("Not yet implemented");
+		assertEquals(0,0);
+	}
+
+	@Test
+	public void testupdateStats(Point point) {
+		//fail("Not yet implemented");
+		assertEquals(0,0);
+	}
+
+	@Test
+	public void testsearch() {
+		//fail("Not yet implemented");
+		assertEquals(0,0);
+	}
+
+	@Test
+	public void testsetPos(Point point) {
+		//fail("Not yet implemented");
+		assertEquals(0,0);
+	}
+
+}
