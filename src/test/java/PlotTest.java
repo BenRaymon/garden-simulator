1b6198e8e1e0b01a8b47df4b0e4cc38e8700662a
Trevor Dolbow <tdolbow@udel.edu>
2021-04-08 18:51:44 -0400

    [feature] added tests to plotTest.java

--- src/test/java/PlotTest.java
+++ src/test/java/PlotTest.java
@@ -5,70 +5,68 @@ import org.junit.Test;
 public class PlotTest {
 	
 	Plot p = new Plot();
+	Point tempPoint = new Point();
+	Point tempPoint2 = new Point();
+	Options op = new Options("st",1,1);
+	Plant testPlant = new Plant(op,0.0,1,"red","large",1.0,"name","name",tempPoint);
+
 	
 	@Test
-	public void testAddCoordinate() {
-		fail("Not yet implemented");
+	public void testAddGetCoordinate() {
+		//fail("Not yet implemented");
+		p.addCoordinate(tempPoint);
+		p.addCoordinate(tempPoint2);
+		assertEquals(p.getCoordinates()[0],tempPoint);
 	}
 	
 	@Test
 	public void testRemoveCoordinate() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		
+		p.removeCoordinate(tempPoint);
+		assertEquals(testPlot.getCoordinates()[0],tempPoint2);
 	}
 	
 	@Test
-	public void testGetCoordinates() {
-		fail("Not yet implemented");
+	public void testRecommendedPlant() {
+		//fail("Not yet implemented");
+		p.addPlantToPlot("plant", testPlant);
+		assertEquals(p.getRecommendedPlants().containsKey("plant"),true);
+		p.removePlantFromPlot("plant");
 	}
 	
-	@Test
-	public void testAddRecommendedPlant() {
-		fail("Not yet implemented");
-	}
+	
 	
 	@Test
-	public void testRemoveRecommendedPlant() {
-		fail("Not yet implemented");
+	public void testPlantToPlot() {
+		//fail("Not yet implemented");
+		p.addPlantToPlot("plant", testPlant);
+		p.addPlantToPlot("plant2", testPlant);
+		assertEquals(p.getPlantsInPlot().containsKey("plant"),true);
+		p.removePlantFromPlot("plant");
 	}
 	
-	@Test
-	public void testGetRecommendedPlants() {
-		fail("Not yet implemented");
-	}
 	
 	@Test
-	public void testAddPlantToPlot() {
-		fail("Not yet implemented");
+	public void testOptions() {
+		//fail("Not yet implemented");
+		p.setOptions(op);
+		assertEquals(p.getOptions(),op);
+
 	}
 	
-	@Test
-	public void testRemovePlantFromPlot() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	public void testgetPlantsInPlot() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	public void testSetOptions() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	public void testGetOptions() {
-		fail("Not yet implemented");
-	}
 	
 	@Test
 	public void testCheckSpread() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		assertEquals(p.checkSpread(),true);
 	}
 	
 	@Test
 	public void testMovePlant() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		assertEquals(p.movePlant(),true);
+		
 	}
 
 }
