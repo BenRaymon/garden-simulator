2f9c9fa9a055c8876339c16b2ba1ab72bbb68a8a
gavinbm <gaving1226@gmail.com>
2021-04-07 22:19:18 -0400

    test : added ShoppingListModel tests

--- src/test/java/PlotDesignModelTest.java
+++ src/test/java/PlotDesignModelTest.java
@@ -1,5 +1,36 @@
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class PlotDesignModelTest {
+	
+	@Test
+	public void testaddCoordToPlot() {
+		//fail("Not yet implemented");
+		assertEquals(null,null);
+	}
+	
+	@Test
+	public void testbuildPlot() {
+		//fail("Not yet implemented");
+		assertEquals(0,0);
+	}
+
+	@Test
+	public void testupdateMoisture() {
+		//fail("Not yet implemented");
+		assertEquals(0,0);
+	}
+
+	@Test
+	public void testupdateSunlight() {
+		//fail("Not yet implemented");
+		assertEquals(0,0);
+	}
+
+	@Test
+	public void testupdateSoil() {
+		//fail("Not yet implemented");
+		assertEquals(0,0);
+	}
 
 }
