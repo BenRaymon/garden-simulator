9c4083dc61c7de599ff65e4ab9cf2bb6d921f7b5
Trevor Dolbow <tdolbow@udel.edu>
2021-04-07 22:04:32 -0400

    [feature] Added unitTests for options class

---
+++ src/test/java/optionsTest.java
@@ -0,0 +1,36 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class OptionsTest{
+	
+	@Test
+	public void testGetSoilType() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSetSoilType() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetSunLevel() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSetSunLevel() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetMoisture() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSetMoisture() {
+		fail("Not yet implemented");
+	}
+	
+}
