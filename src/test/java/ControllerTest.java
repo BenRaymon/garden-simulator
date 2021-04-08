a0783ff89e423061ae4c5816b5c342ff9b71cf1f
Trevor Dolbow <tdolbow@udel.edu>
2021-04-07 22:11:50 -0400

    [feature] Added controller unit tests

---
+++ src/test/java/ControllerTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class ControllerTest{
+	
+	
+	@Test
+	public void testDrawPlot() {
+		fail("Not Yet Implemented");
+	}
+	
+	@Test
+	public void testDragPlot() {
+		fail("Not Yet Implemented");
+	}
+}
