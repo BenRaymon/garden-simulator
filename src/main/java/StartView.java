6f35fb5a40fbc56046291a26585dfd7f0b9e962d
BenRaymon <bendraymon@gmail.com>
2021-04-09 15:36:02 -0400

    feat:added new and load buttons to startview

--- src/main/java/StartView.java
+++ src/main/java/StartView.java
@@ -3,6 +3,8 @@ import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -19,19 +21,33 @@ public class StartView extends View{
 	Text title = new Text("START");
 	
 	public StartView(Stage stage) {
-		StackPane base = new StackPane();
+		GridPane base = new GridPane();
+		base.setHgap(10);
+		base.setVgap(10);
+		base.setAlignment(Pos.CENTER);
+		
+		//Buttons for load and new gardens
+		HBox buttons = new HBox();
+		buttons.setSpacing(100);
+		newGarden = new Button("Create New Garden");
+		newGarden.setMinHeight(50);
+		newGarden.setMinWidth(150);
+		loadGarden = new Button("Load Garden");
+		loadGarden.setMinHeight(50);
+		loadGarden.setMinWidth(150);
+		buttons.getChildren().add(newGarden);
+		buttons.getChildren().add(loadGarden);
+		base.add(buttons, 0, 0);
 		
 		//create a temporary vbox for the name and button
 		VBox temp = new VBox(5);
-		
 		temp.getChildren().add(nextPage);
 		temp.getChildren().add(backPage);
 		temp.getChildren().add(title);
-		
 		temp.setAlignment(Pos.CENTER);
-		base.getChildren().add(temp);
+		base.add(temp, 0, 1);
 		
-		//create and set scene with base StackPane
+		//create and set scene with base
 		scene = new Scene(base, 800, 800);
         stage.setScene(scene);
         stage.show();
