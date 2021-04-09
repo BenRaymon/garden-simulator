e4901c9917a7b0b899743336002512411baa4d1c
BenRaymon <bendraymon@gmail.com>
2021-04-09 14:11:00 -0400

    feat: added main method in controller and main location to build gradle

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -2,6 +2,12 @@ public class Controller{
 	Model model;
 	View view;
 	
+	public static void main(String[] args) {
+		System.out.println("in main");
+		
+	}
+	
+	
 	public boolean drawPlot() {
 		return true;
 	}
