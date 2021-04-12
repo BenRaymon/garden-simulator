b103f5bfaaf8aa963842844197cfc22b9484943e
BenRaymon <bendraymon@gmail.com>
2021-04-12 18:39:03 -0400

    test: fixed all errors in tests

--- src/test/java/PlotDesignModelTest.java
+++ src/test/java/PlotDesignModelTest.java
@@ -2,39 +2,47 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class PlotDesignModelTest {
+
+	PlotDesignModel testModel = new PlotDesignModel();
+	
+	@Test
+	public void testNewPlot() {
+		testModel.newPlot(new Options(1.0, 2.0, 3.0));
+		assertFalse(testModel.getPlots().isEmpty());
+	}
+	
 	
 	@Test
 	public void testaddCoordToPlot() {
 		//fail("Not yet implemented");
-		PlotDesignModel testModel = new PlotDesignModel();
-		assertEquals(testModel.addCoordToPlot(),0);
+		if (testModel.getNumPlots() == 0) {
+			testModel.newPlot(new Options(1.0, 2.0, 3.0));
+		}
+		testModel.addCoordToPlot(0, new Point(1, 1));
+		assertEquals(testModel.getPlots().get(0).getCoordinates().size(),1);
 	}
 	
 	@Test
 	public void testbuildPlot() {
 		//fail("Not yet implemented");
-		PlotDesignModel testModel = new PlotDesignModel();
 		assertEquals(testModel.buildPlot(),0);
 	}
 
 	@Test
 	public void testupdateMoisture() {
 		//fail("Not yet implemented");
-		PlotDesignModel testModel = new PlotDesignModel();
 		assertEquals(testModel.updateMoisture(),0);
 	}
 
 	@Test
 	public void testupdateSunlight() {
 		//fail("Not yet implemented");
-		PlotDesignModel testModel = new PlotDesignModel();
 		assertEquals(testModel.updateSunlight(),0);
 	}
 
 	@Test
 	public void testupdateSoil() {
 		//fail("Not yet implemented");
-		PlotDesignModel testModel = new PlotDesignModel();
 		assertEquals(testModel.updateSoil(),0);
 	}
 
