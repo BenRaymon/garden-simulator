592fb730b4dd5a678fe0dd171c75016d80205fd2
BenRaymon <bendraymon@gmail.com>
2021-04-26 20:53:57 -0400

    test: options tests 100% coverage

--- src/main/java/Options.java
+++ src/main/java/Options.java
@@ -23,7 +23,6 @@ public class Options implements Serializable {
 	}
 	
 	public String toString() {
-		
 		String str = "Soil: [" + soilTypes[0] + ", " + soilTypes[1] + ", " + soilTypes[2] + "] ";
 		str += "Sun: [" + sunLevels[0] + ", " + sunLevels[1] + ", " + sunLevels[2] + "]";
 		str += "Moisture: [" + moistures[0] + ", " + moistures[1] + ", " + moistures[2] + "]";
