0377432affcef4d2fce32c8efe407e88e038dab1
mjcarr <mjcarr@udel.edu>
2021-05-11 23:37:38 -0400

    Adding Lep JavaDoc

--- src/main/java/Lep.java
+++ src/main/java/Lep.java
@@ -24,45 +24,90 @@ public class Lep implements Serializable {
 		countries.add(tempCountry);
 	}
 
-	
+	/**
+	 * Add Lep Host Family
+	 * @param family
+	 */
 	public void addHostFamily(String family) {
 		hostPlantFamily.add(family);
 	}
+	
+	/**
+	 * add Lep Host Name
+	 * @param name
+	 */
 	public void addHostName(String name) {
 		hostPlantName.add(name);
 	}
 
+	/**
+	 * Getter Lep Family()
+	 * @return
+	 */
 	public String getLepFamily() {
 		return lepFamily;
 	}
 
+	/**
+	 * Setter for Lep Family()
+	 * @param lepFamily
+	 */
 	public void setLepFamily(String lepFamily) {
 		this.lepFamily = lepFamily;
 	}
 
+	/**
+	 * Getter for LepName
+	 * @return lepName
+	 */
 	public String getLepName() {
 		return lepName;
 	}
 
+	/**
+	 * Setter for Lep Name
+	 * @param lepName
+	 */
 	public void setLepName(String lepName) {
 		this.lepName = lepName;
 	}
 
+	/**
+	 * Getter for Lep Countries
+	 * @return
+	 */
 	public Set<String> getCountries() {
 		return countries;
 	}
 
+	/**
+	 * Adds lep country
+	 * @param country
+	 */
 	public void addCountry(String country) {
 		countries.add(country);
 	}
 	
+	/**
+	 * Getter for Host Plant family
+	 * @return
+	 */
 	public Set<String> getFamilies(){
 		return hostPlantFamily;
 	}
+	
+	/**
+	 * Getter for Host Plant Name
+	 * @return
+	 */
 	public Set<String> getNames(){
 		return hostPlantName;
 	}
 	
+	/**
+	 * Setter for Host Plant Family
+	 * @param families
+	 */
 	public void setLepFamilies(HashSet<String> families) {
 		this.hostPlantFamily = families;
 	}
